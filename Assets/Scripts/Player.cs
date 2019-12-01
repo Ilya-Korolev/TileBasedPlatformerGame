1a29142bc6301727489dbda161a4a78077af0ca5
Ilya Korolev <39079821+Ilya-Korolev@users.noreply.github.com>
2019-12-01 14:50:02 +0300

    Set up player running state and fix acceleration bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,39 +2,50 @@
 
 public class Player : MonoBehaviour
 {
+    // Congig
     [SerializeField] private float _runSpeed = 10f;
-    [SerializeField] private float _runAcceleration = 10f;
-    [SerializeField] private float _groundDeceleration = 10f;
+    [SerializeField] private float _runAcceleration = 100f;
+    [SerializeField] private float _groundDeceleration = 100f;
 
-    private Rigidbody2D _rigidbody2D = default;
+    // State
+    private bool _isRunning;
+    private bool _hasHorizontalSpeed;
 
-    void Start()
+    // Cached componentReferences
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+    private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         Run();
         Flip();
+
     }
 
     private void Run()
     {
         float horizontalThrow = Input.GetAxis("Horizontal");
+        bool isButtonPressed = horizontalThrow != 0f;
+        float deltaAcceleration = Time.deltaTime * (isButtonPressed ? _runAcceleration : _groundDeceleration);
         Vector2 playerVelocity = _rigidbody2D.velocity;
-        float deltaAcceleration = Time.deltaTime * horizontalThrow != 0f ? _runAcceleration : _groundDeceleration;
 
         playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, horizontalThrow * _runSpeed, deltaAcceleration);
 
         _rigidbody2D.velocity = playerVelocity;
+        _hasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
+        _isRunning = _hasHorizontalSpeed;
+        _animator.SetBool("IsRunning", _isRunning);
     }
 
     private void Flip()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
-
-        if (playerHasHorizontalSpeed)
+        if (_hasHorizontalSpeed)
         {
             float playerHorizontalDirection = Mathf.Sign(_rigidbody2D.velocity.x);
             Vector2 playerScale = new Vector2(playerHorizontalDirection, transform.localScale.y);
