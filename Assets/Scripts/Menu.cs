2b152996e95138525def221830ce10c8ce3d0a76
Ilya Korolev <39079821+Ilya-Korolev@users.noreply.github.com>
2019-12-22 23:44:15 +0300

    Added success screen and menu controller

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartFirstLevel()
+    {
+        var firstLevelIndex = 1;
+
+        SceneManager.LoadScene(firstLevelIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        var menuIndex = 0;
+
+        SceneManager.LoadScene(menuIndex);
+    }
+}
