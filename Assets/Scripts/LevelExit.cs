b8258a969febcd0cc2edf8c7a6501873bd79875a
Ilya Korolev <39079821+Ilya-Korolev@users.noreply.github.com>
2019-12-22 14:29:55 +0300

    Added next level loader

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] private float _levelLoadDelay = 2f;
+    [SerializeField] private float _exitLevelTimeScale = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        StartCoroutine(LoadNextLevel(_levelLoadDelay));
+    }
+
+    private IEnumerator LoadNextLevel(float delay)
+    {
+        Time.timeScale = _exitLevelTimeScale;
+
+        yield return new WaitForSecondsRealtime(delay);
+
+        Time.timeScale = 1f;
+
+        var nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
