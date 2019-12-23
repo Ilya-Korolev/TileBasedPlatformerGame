c850c38163f2e8e026037fe78857761f5292775e
Ilya Korolev <39079821+Ilya-Korolev@users.noreply.github.com>
2019-12-24 01:52:00 +0300

    Added coin pickup SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,8 +2,15 @@
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip _sfxClip = default;
+    [SerializeField] [Range(0f, 1f)] float _sfxVolume = 1f;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        Vector3 cameraPosition = Camera.main.transform.position;
+
+        AudioSource.PlayClipAtPoint(_sfxClip, cameraPosition, _sfxVolume);
+
         Destroy(gameObject);
     }
 }
