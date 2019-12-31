578930d245fcfd1ee2b86a7a63720ea23982bafd
Ilya Korolev <ilya_korolev@zoho.eu>
2019-12-31 20:10:35 +0300

    Added level with rising lava

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [SerializeField] private float _scrollSpeed = 2f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = new Vector2(0f, _scrollSpeed);
+    }
+}
